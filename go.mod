6c7c2cb0cb3a34fe7771fdb83e0e3676d3a71f82
Anselmo José Tavares Ferreira Junior <anselmo.jtfj@gmail.com>
2022-09-04 20:19:13 -0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ajtfj/if711-assignment-4-client
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"net/rpc"
+	"os"
+	"time"
+)
+
+const (
+	SAMPLES_SIZE = 10000
+	ORI          = "A"
+	DEST         = "E"
+)
+
+func main() {
+	host, ok := os.LookupEnv("HOST")
+	if !ok {
+		log.Fatal("undefined HOST")
+	}
+
+	addr := fmt.Sprintf(host)
+	client, err := rpc.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	benchmark(client, ORI, DEST)
+}
+
+func benchmark(client *rpc.Client, ori string, dest string) {
+	var samples []time.Duration
+	for i := 0; i < SAMPLES_SIZE; i++ {
+		log.Printf("sending request to find the shortest path between %s and %s", ori, dest)
+
+		args := ShortestPathArgs{
+			Ori:  ori,
+			Dest: dest,
+		}
+		reply := ShortestPathReply{}
+		start := time.Now()
+		client.Call("Graph.ShortestPath", args, &reply)
+		rtt := time.Since(start)
+
+		samples = append(samples, rtt)
+		log.Printf("shortest path received %v", reply.Path)
+	}
+
+	var mean float64
+	for _, sample := range samples {
+		mean += float64(sample)
+	}
+	mean = mean / float64(len(samples))
+
+	var sd float64
+	for _, sample := range samples {
+		sd += math.Pow((float64(sample) - mean), 2)
+	}
+	sd = math.Sqrt(sd / float64(len(samples)))
+
+	log.Printf("average RTT is %.2f (+- %.2f)", mean, sd)
+}
+
+type ShortestPathArgs struct {
+	Ori  string
+	Dest string
+}
+
+type ShortestPathReply struct {
+	Path []string
+}
